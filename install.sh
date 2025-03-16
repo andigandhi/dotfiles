941a9196a50263de4376678eb9b3ac7b5b8c4071
Andreas Grasser <andi@grasserisen.de>
2025-03-16 20:18:09 +0100

    Updates

--- install.sh
+++ install.sh
@@ -2,24 +2,32 @@
 set -eu
 
 # install git
-sudo apt install git
+sudo apt --yes --force-yes install git
 
 # Clone Repo
 git clone https://github.com/andigandhi/dotfiles $HOME/.dotfiles
 
 # Install dependencies using apt
-sudo apt install sway waybar swaylock fuzzel mako-notifier zathura light zathura-pdf-poppler ssh-askpass lsd udiskie imagemagick zsh
+sudo apt --yes --force-yes install sway waybar swaylock fuzzel mako-notifier zathura light zathura-pdf-poppler ssh-askpass lsd udiskie imagemagick zsh pipewire pipewire-pulse pavucontrol
 
 # install zed
 curl -f https://zed.dev/install.sh | sh
 
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# install Zen Browser
+bash <(curl https://updates.zen-browser.app/appimage.sh)
 
 # link config files
+mkdir -p $HOME/.config/
 ln -s $HOME/.dotfiles/config/* $HOME/.config/
 
 # set zsh as default shell
 sudo chsh -s /usr/bin/zsh $USER
 # Link zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/
+
+# TODOs:
+# - Font
+# - Ly
+#
+
+sway
